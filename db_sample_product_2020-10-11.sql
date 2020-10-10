1d2b500f4d9dd0b81ef3fe393c2987c3c448c9cd
Muhammad Nuridin <muhammad.nuridin@rajamobil.net>
2020-10-11 06:30:14 +0700

    add db

---
+++ db_sample_product_2020-10-11.sql
@@ -0,0 +1,86 @@
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 4541
+#
+# http://www.sequelpro.com/
+# https://github.com/sequelpro/sequelpro
+#
+# Host: 127.0.0.1 (MySQL 5.7.27)
+# Database: db_sample_product
+# Generation Time: 2020-10-10 23:29:52 +0000
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table app_master_barang
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `app_master_barang`;
+
+CREATE TABLE `app_master_barang` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `kode_barang` int(11) DEFAULT NULL,
+  `nama_barang` varchar(50) DEFAULT NULL,
+  `harga` int(11) DEFAULT '0',
+  `status` varchar(30) DEFAULT '1' COMMENT '0 = expired, 1 ready',
+  `expired_date` date DEFAULT NULL,
+  `barcode` varchar(100) DEFAULT NULL,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `kode_barang` (`kode_barang`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+LOCK TABLES `app_master_barang` WRITE;
+/*!40000 ALTER TABLE `app_master_barang` DISABLE KEYS */;
+
+INSERT INTO `app_master_barang` (`id`, `kode_barang`, `nama_barang`, `harga`, `status`, `expired_date`, `barcode`, `created_at`)
+VALUES
+	(1,989432,'TEST BARANG',0,'1','2020-12-30',NULL,'2020-09-06 00:00:00'),
+	(2,989433,'TEST BARANG 2',0,'1','2020-10-17',NULL,'2020-09-06 21:59:42'),
+	(3,989434,'TEST BARANG 3',0,'0','2020-09-30',NULL,'2020-09-06 21:59:42');
+
+/*!40000 ALTER TABLE `app_master_barang` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Dump of table app_users
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `app_users`;
+
+CREATE TABLE `app_users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `role` varchar(20) DEFAULT NULL,
+  `nama` varchar(50) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `password` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `users_role` (`role`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+LOCK TABLES `app_users` WRITE;
+/*!40000 ALTER TABLE `app_users` DISABLE KEYS */;
+
+INSERT INTO `app_users` (`id`, `role`, `nama`, `email`, `password`)
+VALUES
+	(7,'admin','admin','admin@gmail.com','202cb962ac59075b964b07152d234b70');
+
+/*!40000 ALTER TABLE `app_users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
